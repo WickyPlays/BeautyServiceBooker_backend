f9250150a74d2a57efe6758d55289f7a7f7002d7
WickyPlays <nguyenthienbao8601@gmail.com>
2024-10-13 08:17:19 +0700

    Login auth

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require('dotenv').config();
 
-require('./db')
+require("./db");
 
-var userRoutes = require('./routes/user_route');
-var locationRoutes = require('./routes/location_route');
-var promoCodeRoutes = require('./routes/promocode_route');
-var serviceRoutes = require('./routes/service_route');
-var billRoutes = require('./routes/bill_route');
-var billServiceRoutes = require('./routes/billservice_route');
+var authRoutes = require("./routes/auth_route");
+var userRoutes = require("./routes/user_route");
+var locationRoutes = require("./routes/location_route");
+var promoCodeRoutes = require("./routes/promocode_route");
+var serviceRoutes = require("./routes/service_route");
+var billRoutes = require("./routes/bill_route");
+var billServiceRoutes = require("./routes/billservice_route");
+var authMiddleware = require("./middlewares/auth_middleware");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/users', userRoutes);
-app.use('/locations', locationRoutes);
-app.use('/promocodes', promoCodeRoutes);
-app.use('/services', serviceRoutes);
-app.use('/bills', billRoutes);
-app.use('/billservices', billServiceRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", authMiddleware, userRoutes);
+app.use("/locations", locationRoutes);
+app.use("/promocodes", promoCodeRoutes);
+app.use("/services", serviceRoutes);
+app.use("/bills", authMiddleware, billRoutes);
+app.use("/billservices", authMiddleware, billServiceRoutes);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ routes/auth_route.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/UserModel");
+
+router.post("/register", async (req, res) => {
+  const { name, email, password, phone, loginMethod } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      loginMethod,
+    });
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send("Invalid email or password");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).send("Invalid email or password");
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, username: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).send({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
